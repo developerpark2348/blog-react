be8653dfaa216029e190aa8bf4e849c0fac605d0
developerpark2348 <developerpark2348@gmail.com>
2020-12-26 17:37:11 +0900

    add 글
    add 글 using useState
    useState('글')는 ['글', '글 변경함수']배열
    를 만들기에 ES6에 추가된
    [a, b] = useState('글')을 사용하여
    a = '글' b='글 변경함수'를 만든다.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [글제목, 글제목변경] = useState('react 강의');
+  let [글제목2, 글제목변경2] = useState('typescript 강의');
+  let [글제목3, 글제목변경3] = useState('남자 코트 코디');
+  
   return (
     <div className="App">
-      <div className="">
-        asgdsad
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <div className="list">
+        <h3> { 글제목 } </h3>
+        <p>12월 26일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { 글제목2 } </h3>
+        <p>12월 26일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { 글제목3 } </h3>
+        <p>12월 26일 발행</p>
+        <hr/>
       </div>
     </div>
   );

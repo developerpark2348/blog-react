85860ad29259630680014f7070eee6781a6b3576
developerpark2348 <developerpark2348@gmail.com>
2020-12-27 10:45:19 +0900

    add like button
    좋아요 버튼을 추가하고 버튼 클릭시 +1이 되게 usestate의 변경함수를 사용함.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
+/* eslint-disable */
+
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  let [글제목, 글제목변경] = useState('react 강의');
-  let [글제목2, 글제목변경2] = useState('typescript 강의');
-  let [글제목3, 글제목변경3] = useState('남자 코트 코디');
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', 'typescript강의', 'react 강의']);
+  let [따봉, 따봉변경] = useState(0);
+  
+  function 제목바꾸기(){
+    글제목변경(['여자 코트 추천', 'typescript강의', 'react 강의']);
+  }
   
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
+      <button onClick={ 제목바꾸기 }>버튼</button>
       <div className="list">
-        <h3> { 글제목 } </h3>
+        <h3> { 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉+1) } }>♥</span> {따봉} </h3>
         <p>12월 26일 발행</p>
         <hr/>
       </div>
       <div className="list">
-        <h3> { 글제목2 } </h3>
+        <h3> { 글제목[1] } </h3>
         <p>12월 26일 발행</p>
         <hr/>
       </div>
       <div className="list">
-        <h3> { 글제목3 } </h3>
+        <h3> { 글제목[2] } </h3>
         <p>12월 26일 발행</p>
         <hr/>
       </div>

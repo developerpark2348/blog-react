84e5697e45f0c834473ff54ff94245724850e068
developerpark2348 <developerpark2348@gmail.com>
2020-12-27 17:26:39 +0900

    input받기 및 input state에 저장하기.
    input을 받아 state변환 함수로 input안의 데이터 e.target.value를 state변환함수(e.target.value)로 넣어준다.

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ function App() {
 
   let [글제목, 글제목변경] = useState(['남자 코트 추천', 'typescript강의', 'react 강의']);
   let [따봉, 따봉변경] = useState(0);
-  let [modal, modal변경] = useState(false);
+  let [modal, modal변경] = useState(true);
   let [누른제목, 누른제목변경] = useState(0);
+
+  let [입력값, 입력값변경] = useState('');
   
   return (
     <div className="App">
@@ -19,7 +21,7 @@ function App() {
       { // i는 반복문이 돌 때마다 0,1,2가 되는 변수 (변수의 가장 끝 값)
         글제목.map((글, i)=>{
           return (
-          <div className="list">
+          <div className="list" key={i}>
             <h3 onClick={ ()=>{ 누른제목변경(i) } }> { 글 } <span onClick={ ()=>{ 따봉변경(따봉+1) } }>👍</span> {따봉} </h3>
             <p>12월 26일 발행</p>
             <hr/>
@@ -27,6 +29,8 @@ function App() {
           )
         })
       }
+
+      <input onChange={ (e)=> { 입력값변경(e.target.value) } } /> 
 
       <button onClick={ ()=> { modal변경(!modal) } }>열고닫기</button> 
       {/* onClick={ ()=>{ modal === true ? modal변경(false) : modal변경(true) } }도 가능함. */}

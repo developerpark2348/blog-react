b7593331d8ee505644e904ba93a2950fef3c39fd
developerpark2348 <developerpark2348@gmail.com>
2020-12-27 17:58:27 +0900

    글 발행기능 추가
    배열.unshift(추가할 값); 배열 가장 앞에 추가할 값을 추가해주는 함수
    배열.unshift(추가할 값);

--- src/App.js
+++ src/App.js
@@ -30,7 +30,14 @@ function App() {
         })
       }
 
-      <input onChange={ (e)=> { 입력값변경(e.target.value) } } /> 
+      <div className="publish">
+        <input onChange={ (e)=>{ 입력값변경(e.target.value) } }/>
+        <button onClick={ ()=>{
+          var arrayCopy = [...글제목]
+          arrayCopy.unshift(입력값);
+          글제목변경( arrayCopy )
+        } }>저장</button>
+      </div>
 
       <button onClick={ ()=> { modal변경(!modal) } }>열고닫기</button> 
       {/* onClick={ ()=>{ modal === true ? modal변경(false) : modal변경(true) } }도 가능함. */}

02b443109f1cbc4aec90f06af6e3e59c48afb74b
developerpark2348 <developerpark2348@gmail.com>
2020-12-27 16:56:19 +0900

    make modal with h3

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
 
   let [글제목, 글제목변경] = useState(['남자 코트 추천', 'typescript강의', 'react 강의']);
   let [따봉, 따봉변경] = useState(0);
-
   let [modal, modal변경] = useState(false);
+  let [누른제목, 누른제목변경] = useState(0);
   
   return (
     <div className="App">
@@ -16,11 +16,11 @@ function App() {
         <div>개발 Blog</div>
       </div>
 
-      {
-        글제목.map((글)=>{
+      { // i는 반복문이 돌 때마다 0,1,2가 되는 변수 (변수의 가장 끝 값)
+        글제목.map((글, i)=>{
           return (
           <div className="list">
-            <h3> { 글 } <span onClick={ ()=>{ 따봉변경(따봉+1) } }>👍</span> {따봉} </h3>
+            <h3 onClick={ ()=>{ 누른제목변경(i) } }> { 글 } <span onClick={ ()=>{ 따봉변경(따봉+1) } }>👍</span> {따봉} </h3>
             <p>12월 26일 발행</p>
             <hr/>
           </div>
@@ -33,7 +33,7 @@ function App() {
 
       {
         modal === true
-        ? <Modal 글제목={글제목}></Modal>
+        ? <Modal 글제목={글제목} 누른제목={누른제목}></Modal>
         : null
       }
 
@@ -45,7 +45,7 @@ function App() {
 function Modal(props){
   return (
     <div className="modal">
-        <h2>{ props.글제목[1] } </h2>
+        <h2>{ props.글제목[props.누른제목] } </h2>
         <p>날짜</p>
         <p>상세내용</p>
     </div>
